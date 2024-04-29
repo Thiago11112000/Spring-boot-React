579896fd934744e0755d5df1425396cfa7fa3024
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-29 17:01:06 -0300

    Serviço para cadastrar produtos

--- Api-Back/Produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ Api-Back/Produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -4,6 +4,8 @@ import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +14,25 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
     //Método para listar todos os produtos
 
     public Iterable<ProdutoModelo> listar() {
         return pr.findAll();
     }
+   // Método para cadastrar Produtos
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+        if(pm.getNome().equals("")){
+        rm.setMensagem("O nome do produto é obrigatório!");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        } else if (pm.getMarca().equals("")){
+          rm.setMensagem(("O nome da marca é obrigatório!"));
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+        }
+    }
+
+
 }
