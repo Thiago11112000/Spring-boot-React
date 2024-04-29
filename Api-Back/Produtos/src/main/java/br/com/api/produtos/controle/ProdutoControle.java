a5d2fcb2e855010c95083ce59c1cfed477a31ac9
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-29 11:20:24 -0300

    Rota para listar produtos

--- Api-Back/Produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ Api-Back/Produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,14 +1,24 @@
 package br.com.api.produtos.controle;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProdutoControle {
 
-    @GetMapping("/")
-    public String rota(){
-        return "Api de produtos funcionando!";
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
     }
 
-
+    @GetMapping("/")
+    public String rota() {
+        return "Api de produtos funcionando!";
+    }
 }
