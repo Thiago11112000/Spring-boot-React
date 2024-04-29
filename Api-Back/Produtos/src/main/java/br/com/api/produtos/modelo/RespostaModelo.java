c4c7fcaa680d06f7f20fd247d9b4c4a672040f84
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-29 09:24:28 -0300

    Modelo para validar requisições(response)

--- Api-Back/Produtos/src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
+++ Api-Back/Produtos/src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
@@ -1,5 +1,13 @@
 package br.com.api.produtos.modelo;
 
-public class RespostaModelo {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
+@Component
+@Getter
+@Setter
+public class RespostaModelo {
+ private String mensagem;
 }
