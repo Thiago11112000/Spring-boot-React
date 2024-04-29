fe50b2335fa3ab8331d4da046b53f5090f1749ae
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-29 17:17:11 -0300

    Rota para cadastrar Produtos

--- Api-Back/Produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ Api-Back/Produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -3,7 +3,10 @@ package br.com.api.produtos.controle;
 import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +15,16 @@ public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
 
+    @PostMapping("/cadastrar")
+       public ResponseEntity<?> cadastrar( @RequestBody ProdutoModelo pm){
+         return ps.cadastrar(pm);
+    }
+
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {
         return ps.listar();
     }
+
 
     @GetMapping("/")
     public String rota() {
